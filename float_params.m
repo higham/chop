e763a003a03e497b927d359d9055ce38ba8110bb
Nick Higham <nick.higham@manchester.ac.uk>
2020-11-18 09:42:28 +0000

    Updated to latest float_params.

--- float_params.m
+++ float_params.m
@@ -13,6 +13,7 @@ function [u,xmins,xmin,xmax,p,emins,emin,emax] = float_params(prec)
 %   where prec is one of 
 %    'b', 'bfloat16'           - bfloat16,
 %    'h', 'half', 'fp16'       - IEEE half precision,
+%    't', 'tf32'               - NVIDIA tf32,
 %    's', 'single', 'fp32'     - IEEE single precision,
 %    'd', double', 'fp64'      - IEEE double precision (the default),
 %    'q', 'quadruple', 'fp128' - IEEE quadruple precision.
@@ -20,43 +21,48 @@ function [u,xmins,xmin,xmax,p,emins,emin,emax] = float_params(prec)
 %   s * 2^e * d_0.d_1d_2...d_{t-1} where s = 1 or -1, e is the exponent
 %   and each d_i is 0 or 1, with d_0 = 1 for normalized numbers.
 %   With no input and output arguments, FLOAT_PARAMS prints a table showing
-%   all the parameters for all five precisions.
+%   all the parameters for all the precisions.
 %   Note: xmax and xmin are not representable in double precision for
 %   'quadruple'.
 
 %   Author: Nicholas J. Higham.
 
 % References:
-% [1] IEEE Standard for Floating-Point Arithmetic, IEEE Std 754-2008 (revision 
-%      of IEEE Std 754-1985), 58, IEEE Computer Society, 2008; pages 8,
-%      13. https://ieeexplore.ieee.org/document/4610935
+% [1] IEEE Standard for Floating-Point Arithmetic, IEEE Std 754-2019
+%     (revision of IEEE Std 754-2008), The Institute of Electrical and
+%     Electronics Engineers 2019.
 % [2] Intel Corporation, BFLOAT16---hardware numerics definition,  Nov. 2018, 
-%      White paper. Document number 338302-001US.
-%      https://software.intel.com/en-us/download/bfloat16-hardware-numerics-definition}.
-% [3] https://stackoverflow.com/questions/44873802/what-is-tf-bfloat16-truncated-16-bit-floating-point
-% [4] https://en.wikipedia.org/wiki/Bfloat16_floating-point_format
+%     White paper. Document number 338302-001US.
+% [3] Shibo Wang and Pankaj Kanwar, BFloat16: The Secret To High Performance 
+%     on Cloud TPUs, 2019.
+% [4] https://en.wikipedia.org/wiki/Bfloat16_floating-point_format.
+% [5] NVIDIA Corporation, NVIDIA A100 Tensor Core GPU Architecture, 2020.
 
 if nargin < 1 && nargout < 1
-   precs = 'bhsdq';
-   fprintf(['        u        xmins       xmin       xmax    p    emin    ' ...
-            'emax\n'])
-   fprintf('    -------------------------------------------------------------\n')
+   precs = 'bhtsdq';
+   fprintf(['        u        xmins      xmin       xmax      p   ' ...
+            ' emins     emin    emax\n'])
+   fprintf('    ---------------------------------------------------------')
+   fprintf('---------------\n')
    for j = 1:length(precs)
-      [u,xmins,xmin,xmax,p,emin,emax] = float_params(precs(j));
-      fprintf('%s: %9.2e  %9.2e  %9.2e  %9.2e  %3.0f  %5.0f  %6.0f\n',...
-               precs(j),u,xmins,xmin,xmax,p,emin,emax)
+      [u,xmins,xmin,xmax,p,emins,emin,emax] = float_params(precs(j));
+      fprintf('%s: %9.2e  %9.2e  %9.2e  %9.2e  %3.0f  %7.0f  %7.0f  %6.0f\n',...
+               precs(j),u,xmins,xmin,xmax,p,emins,emin,emax)
    end
    clear u, return
 end   
 
 if nargin < 1, prec = 'd'; end
 
-if ismember(prec, {'h','half','fp16'})
-    % Significand: 10 bits plus 1 hidden. Exponent: 5 bits.
-    p = 11; emax = 15;
-elseif ismember(prec, {'b','bfloat16'})
+if ismember(prec, {'b','bfloat16'})
     % Significand: 7 bits plus 1 hidden. Exponent: 8 bits.
     p = 8; emax = 127;  
+elseif ismember(prec, {'h','half','fp16'})
+    % Significand: 10 bits plus 1 hidden. Exponent: 5 bits.
+    p = 11; emax = 15;
+elseif ismember(prec, {'t','tf32'})
+    % Significand: 10 bits plus 1 hidden. Exponent: 8 bits.
+    p = 11; emax = 127;  
 elseif ismember(prec, {'s','single','fp32'})
     % Significand: 23 bits plus 1 hidden. Exponent: 8 bits.
     p = 24; emax = 127;
